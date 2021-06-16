e6e4e32bc4f8552fd3146c6c502c45c5d5a5b49d
han1202012 <octopus_truth@163.com>
2021-06-16 23:27:31 +0800

    update

--- app/src/main/java/com/example/plugin_hook/MainActivity.java
+++ app/src/main/java/com/example/plugin_hook/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.plugin_hook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 获取按钮 , 并未按钮组件设置点击事件
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button OnClickListener onClick");
+            }
+        });
     }
+
+
+    private void hook(){
+
+    }
+
 }
