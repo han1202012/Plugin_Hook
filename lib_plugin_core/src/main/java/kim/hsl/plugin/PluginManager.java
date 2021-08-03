0f4acd68ac8f1a7547022dcd7f4cabcc8c97a9ad
hanshuliang <octopus_truth@163.com>
2021-08-03 13:28:41 +0800

    update

--- lib_plugin_core/src/main/java/kim/hsl/plugin/PluginManager.java
+++ lib_plugin_core/src/main/java/kim/hsl/plugin/PluginManager.java
@@ -2,9 +2,11 @@ package kim.hsl.plugin;
 
 import android.content.Context;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 
 import dalvik.system.DexClassLoader;
+import dalvik.system.PathClassLoader;
 
 /**
  * 使用 Hook 实现的插件使用入口
@@ -62,13 +64,13 @@ public class PluginManager {
 
     private void loadApk() {
         // 插件包的绝对路径 ,  /data/data/< package name >/files/
-        String apkPath = mBase.getFilesDir().getAbsolutePath() + "x.apk";
+        String apkPath = mBase.getFilesDir().getAbsolutePath() + "plugin.apk";
         // 加载插件包后产生的缓存文件路径
-        // data/data/< package name >/app_plugin_cache
+        // /data/data/< package name >/app_plugin_cache/
         String cachePath =
                 mBase.getDir("plugin_cache", Context.MODE_PRIVATE).getAbsolutePath();
         // 创建类加载器
-        DexClassLoader dexClassLoader =
+        DexClassLoader plugin_dexClassLoader =
                 new DexClassLoader(
                         apkPath,                // 插件包路径
                         cachePath,              // 插件包加载时产生的缓存路径
@@ -78,11 +80,21 @@ public class PluginManager {
 
         // 1. 反射 " 插件包 " 应用的 dexElement
 
+        // 执行步骤 :
+        // ① 反射获取 BaseDexClassLoader.class
+        // ② 反射获取 BaseDexClassLoader.calss 中的 private final DexPathList pathList 成员字段
+        // ③ 反射获取 DexClassLoader 类加载器中的 DexPathList pathList 成员对象
+        // ④ 反射获取 DexPathList.class
+        // ⑤ 反射获取 DexPathList.class 中的 private Element[] dexElements 成员变量的 Field 字段对象
+        // ⑥ 反射获取 DexPathList 对象中的 private Element[] dexElements 成员变量对象
+
+        // ① 反射获取 BaseDexClassLoader.class
         // 通过反射获取插件包中的 dexElements
         // 这种类加载是合并类加载 , 将所有的 Dex 文件 , 加入到应用的 dex 文件集合中
         //  可参考 dex 加固 , 热修复 , 插装式插件化 的实现步骤
         // 反射出 BaseDexClassLoader 类 , PathClassLoader 和 DexClassLoader
         //  都是 BaseDexClassLoader 的子类
+        // 参考 https://www.androidos.net.cn/android/9.0.0_r8/xref/libcore/dalvik/src/main/java/dalvik/system/BaseDexClassLoader.java
         Class<?> baseDexClassLoaderClass = null;
         try {
             baseDexClassLoaderClass = Class.forName("dalvik.system.BaseDexClassLoader");
@@ -90,35 +102,178 @@ public class PluginManager {
             e.printStackTrace();
         }
 
-        // 获取 BaseDexClassLoader 类中的
-        //  private final DexPathList pathList 成员变量
-        Field pathListField = null;
+        // ② 反射获取 BaseDexClassLoader.calss 中的 private final DexPathList pathList 成员字段
+        Field plugin_pathListField = null;
         try {
-            pathListField = baseDexClassLoaderClass.getDeclaredField("pathList");
+            plugin_pathListField = baseDexClassLoaderClass.getDeclaredField("pathList");
             // 设置属性的可见性
-            pathListField.setAccessible(true);
+            plugin_pathListField.setAccessible(true);
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
 
-        // 获取 DexClassLoader dexClassLoader 类加载器的 DexPathList pathList 成员变量
+        // ③ 反射获取 plugin_dexClassLoader 类加载器中的 DexPathList pathList 成员对象
+        // 根据 Field 字段获取 成员变量
         //  DexClassLoader 继承了 BaseDexClassLoader, 因此其内部肯定有
         //  private final DexPathList pathList 成员变量
-        Object pathListObject = null;
+        Object plugin_pathListObject = null;
         try {
-            pathListObject = pathListField.get(dexClassLoader);
+            plugin_pathListObject = plugin_pathListField.get(plugin_dexClassLoader);
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
 
+        // ④ 获取 DexPathList.class
+
+        // DexPathList 类中有 private Element[] dexElements 成员变量
+        // 通过反射获取该成员变量
+        // 参考 https://www.androidos.net.cn/android/9.0.0_r8/xref/libcore/dalvik/src/main/java/dalvik/system/DexPathList.java
+
+        // 获取 DexPathList pathList 成员变量的字节码类型 ( 也可以通过反射获得 )
+        // 获取的是 DexPathList.class
+        Class<?> plugin_dexPathListClass = plugin_pathListObject.getClass();
+
+        // ⑤ 反射获取 DexPathList.class 中的 private Element[] dexElements 成员变量的 Field 字段对象
+        Field plugin_dexElementsField = null;
+        try {
+            plugin_dexElementsField = plugin_dexPathListClass.getDeclaredField("dexElements");
+            // 设置属性的可见性
+            plugin_dexElementsField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        // ⑥ 反射获取 DexPathList 对象中的 private Element[] dexElements 成员变量对象
+        Object plugin_dexElementsObject = null;
+        try {
+            plugin_dexElementsObject = plugin_dexElementsField.get(plugin_pathListObject);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
         // 2. 反射 " 宿主 " 应用的 dexElement
+        // 执行步骤 :
+        // ① 反射获取 BaseDexClassLoader.class
+        // ② 反射获取 BaseDexClassLoader.calss 中的 private final DexPathList pathList 成员字段
+        // ③ 反射获取 PathClassLoader 类加载器中的 DexPathList pathList 成员对象
+        // ④ 反射获取 DexPathList.class
+        // ⑤ 反射获取 DexPathList.class 中的 private Element[] dexElements 成员变量的 Field 字段对象
+        // ⑥ 反射获取 DexPathList 对象中的 private Element[] dexElements 成员变量对象
 
-        // 获取 DexPathList pathList 成员变量的字节码类型
-        Class<?> dexPathListClass = pathListObject.getClass();
+        // ① 反射获取 BaseDexClassLoader.class
+        Class<?> host_baseDexClassLoaderClass = null;
+        try {
+            host_baseDexClassLoaderClass = Class.forName("dalvik.system.BaseDexClassLoader");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // ② 反射获取 BaseDexClassLoader.calss 中的 private final DexPathList pathList 成员字段
+        Field host_pathListField = null;
+        try {
+            host_pathListField = host_baseDexClassLoaderClass.getDeclaredField("pathList");
+            // 设置属性的可见性
+            host_pathListField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        // ③ 反射获取 DexClassLoader 类加载器中的 DexPathList pathList 成员对象
+        // 根据 Field 字段获取 成员变量
+        //  DexClassLoader 继承了 BaseDexClassLoader, 因此其内部肯定有
+        //  private final DexPathList pathList 成员变量
+        PathClassLoader host_pathClassLoader = (PathClassLoader) mBase.getClassLoader();
+        Object host_pathListObject = null;
+        try {
+            host_pathListObject = host_pathListField.get(host_pathClassLoader);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        // ④ 获取 DexPathList.class
+
+        // DexPathList 类中有 private Element[] dexElements 成员变量
+        // 通过反射获取该成员变量
+        // 参考 https://www.androidos.net.cn/android/9.0.0_r8/xref/libcore/dalvik/src/main/java/dalvik/system/DexPathList.java
+
+        // 获取 DexPathList pathList 成员变量的字节码类型 ( 也可以通过反射获得 )
+        // 获取的是 DexPathList.class
+        Class<?> host_dexPathListClass = host_pathListObject.getClass();
+
+        // ⑤ 反射获取 DexPathList.class 中的 private Element[] dexElements 成员变量的 Field 字段对象
+        Field host_dexElementsField = null;
+        try {
+            host_dexElementsField = host_dexPathListClass.getDeclaredField("dexElements");
+            // 设置属性的可见性
+            host_dexElementsField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        // ⑥ 反射获取 DexPathList 对象中的 private Element[] dexElements 成员变量对象
+        Object host_dexElementsObject = null;
+        try {
+            host_dexElementsObject = host_dexElementsField.get(host_pathListObject);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
 
 
+        // 3. 合并 “插件包“ 与 “宿主“ 中的 Element[] dexElements
 
+        // 将两个 Element[] dexElements 数组合并 ,
+        //  合并完成后 , 设置到 PathClassLoader 中的
+        //  DexPathList pathList 成员的 Element[] dexElements 成员中
+
+        // 获取 “宿主“ 中的 Element[] dexElements 数组长度
+        int host_dexElementsLength = Array.getLength(host_dexElementsObject);
+        // 获取 “插件包“ 中的 Element[] dexElements 数组长度
+        int plugin_dexElementsLength = Array.getLength(plugin_dexElementsObject);
+
+        // 获取 Element[] dexElements 数组中的 , 数组元素的 Element 类型
+        // 获取的是 Element.class
+        Class<?> elementClazz = host_dexElementsObject.getClass().getComponentType();
+
+        // 合并后的 Element[] dexElements 数组长度
+        int new_dexElementsLength = plugin_dexElementsLength + host_dexElementsLength;
+
+        // 创建 Element[] 数组 , elementClazz 是 Element.class 数组元素类型
+        Object newElementsArray = Array.newInstance(elementClazz, new_dexElementsLength);
+
+        //  为新的 Element[] newElementsArray 数组赋值
+        //      先将 “插件包“ 中的 Element[] dexElements 数组放入到新数组中
+        //      然后将 “宿主“ 中的 Element[] dexElements 数组放入到新数组中
+        for (int i = 0; i < new_dexElementsLength; i++) {
+            if (i < plugin_dexElementsLength) {
+                // “插件包“ 中的 Element[] dexElements 数组放入到新数组中
+                Array.set(newElementsArray, i, Array.get(plugin_dexElementsObject, i));
+            } else {
+                //  “宿主“ 中的 Element[] dexElements 数组放入到新数组中
+                Array.set(newElementsArray, i, Array.get(host_dexElementsObject, i - plugin_dexElementsLength));
+            }
+        }
+
+
+        // 4. 重新设置 PathClassLoader 中的 DexPathList pathList 成员的 Element[] dexElements 属性值
+        Field elementsFiled = null;
+        try {
+            elementsFiled = host_pathListObject.getClass().getDeclaredField("dexElements");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        elementsFiled.setAccessible(true);
+
+        //  设置 DexPathList pathList 的 Element[] dexElements 属性值
+        //      host_pathListObject 是原来的属性值
+        //      newElementsArray 是新的合并后的 Element[] dexElements 数组
+        //  注意 : 这里也可以使用 host_dexElementsField 字段进行设置
+        try {
+            elementsFiled.set(host_pathListObject, newElementsArray);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
     }
 
